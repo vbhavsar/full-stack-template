e2ddde2903d84eff4e8aa270132ad9969449827b
Vishal Bhavsar <vbhavsar@gmail.com>
2024-01-29 19:00:06 -0500

    Add frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="mt-4 text-center">
+      <h1 className="text-3xl font-bold underline">
+        ~Welcome to the full stack template~
+      </h1>
+    </div>
+  );
+}
+
+export default App;
